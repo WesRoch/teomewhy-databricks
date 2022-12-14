98658cafbcc2b0aaaaac5b3d51d0398c9fe5e49f
WesRoch <WesRoch>
2022-12-14 01:00:47 +0000

    more sql functions

--- sql-databricks.sql
+++ sql-databricks.sql
@@ -49,4 +49,86 @@ FROM
 
 -- COMMAND ----------
 
+SELECT
+  *
+FROM
+  silver_olist.pedido
+WHERE
+  descSituacao = 'delivered'
+  
+-- selecionar todas as colunas da tabela pedido AONDE a situação é entregue 'delivered'
+
+-- COMMAND ----------
+
+SELECT
+  *
+FROM
+  silver_olist.pedido
+WHERE
+  descSituacao = 'canceled'
+  
+  -- neste caso, como tem menos de 1000 linhas, exatos 625, ele retornou todas as linhas da tabela de acordo com a querie.
+
+-- COMMAND ----------
+
+SELECT
+  *
+FROM
+  silver_olist.pedido
+WHERE
+  descSituacao = 'shipped'
+  AND YEAR (dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT
+  *
+FROM
+  silver_olist.pedido
+WHERE
+  (
+    descSituacao = 'shipped'
+    OR descSituacao = 'canceled'
+  )
+  AND YEAR (dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT
+  *
+FROM
+  silver_olist.pedido
+WHERE
+  (
+    descSituacao = 'shipped'
+    OR 'canceled'
+  )
+  AND YEAR (dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT
+  *
+FROM
+  silver_olist.pedido
+WHERE
+  descSituacao IN ('shipped', 'canceled')
+  AND YEAR (dtPedido) = '2018'
+
+-- COMMAND ----------
+
+SELECT
+  *,
+  DATEDIFF (dtEstimativaEntrega, dtAprovado) AS diasAtraso
+FROM
+  silver_olist.pedido
+WHERE
+  descSituacao IN ('shipped', 'canceled')
+  AND YEAR(dtPedido) = '2018'
+  AND DATEDIFF(dtEstimativaEntrega, dtAprovado) > 30
+ORDER BY
+  diasAtraso
+
+-- COMMAND ----------
+
 
